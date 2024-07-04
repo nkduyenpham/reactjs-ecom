26909e7b18027b13c83c156266755d1cb1557cc1
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-07-04 11:42:16 +0800

    innitial project setup, design navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+const Navbar = () => {
+
+    const [menu, setMenu] = useState("shop");
+
+    return (
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src={logo} alt="cart logo" />
+                <p>FASHION</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={() => { setMenu("shop") }}>Shop{menu === "shop" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("men") }}>Men{menu === "men" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("women") }}>Women{menu === "women" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("kids") }}>Kids{menu === "kids" ? <hr /> : <></>}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="cart icon" />
+                <div className="nav-cart-count">0</div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
