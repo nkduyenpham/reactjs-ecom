98a52b2be844a35e603f6d185c57b3b520fa2f0b
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-07-05 11:29:46 +0800

    setup product details page

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Breadcrum.css'
-import arrow_icon from '../Assets/breadscrum_arrow.png'
+import arrow_icon from '../Assets/breadcrum_arrow.png'
 
 const Breadscrum = (props) => {
     const { product } = props;
     return (
-        <div className='breadscrum'>
+        <div className='breadcrum'>
             HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
         </div>
     )
--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import './Item.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Item.css';
 
 const Item = (props) => {
     return (
         <div className="item">
-            <Link to={`/product/${props.id}`}></Link><img src={props.image} alt="" />
+            <Link to={`/product/${props.id}`}>
+                <img src={props.image} alt={props.name} />
+            </Link>
             <p>{props.name}</p>
             <div className="item-prices">
                 <div className="item-price-new">${props.new_price}</div>
-                <div className="item-price-old">
-                    ${props.old_price}
-                </div>
+                <div className="item-price-old">${props.old_price}</div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Item 
+export default Item;
