83aae2b3d026ad798f12aad23649ec9faed72cdc
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-07-05 14:23:56 +0800

    setup cart total and promote code in cart page, setup total cart item on navbar

---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,67 @@
+import React, { useContext } from 'react'
+import './CartItems.css'
+import { ShopContext } from '../../Context/ShopContext'
+import remove_icon from '../Assets/cart_cross_icon.png'
+
+const CartItems = () => {
+    const { getTotalCartAmount, all_product, cartItems, removeFromCart } = useContext(ShopContext)
+    return (
+        <div className='cartitems'>
+            <div className="cartitems-format-main">
+                <p>Products</p>
+                <p>Title</p>
+                <p>Price</p>
+                <p>Quantity</p>
+                <p>Total</p>
+                <p>Remove</p>
+            </div>
+            <hr />
+            {all_product.map((e) => {
+                if (cartItems[e.id] > 0) {
+                    return (
+                        <div className="cartitems-format cartitems-format-main" key={e.id}>
+                            <img src={e.image} alt="" className='cartitems-product-icon' />
+                            <p>{e.name}</p>
+                            <p>${e.new_price}</p>
+                            <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                            <p>${e.new_price * cartItems[e.id]}</p>
+                            <img className='cartitems-remove-icon' src={remove_icon} onClick={() => { removeFromCart(e.id) }} alt="" />
+                        </div>
+                    );
+                }
+                return null;
+            })}
+            <div className="cartitems-down">
+                <div className="cartitems-total">
+                    <h1>Cart Total</h1>
+                    <div>
+                        <div className="cartitems-total-item">
+                            <p>Subtotal</p>
+                            <p>${getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <p>Shipping Fee</p>
+                            <p>Free</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <h3>Total</h3>
+                            <h3>${getTotalCartAmount()}</h3>
+                        </div>
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+                <div className="cartitems-promotecode">
+                    <p>If you have promote code, Enter it here</p>
+                    <div className="cartitems-promotebox">
+                        <input type="text" placeholder='promote code' />
+                        <button>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CartItems
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 
 const Navbar = () => {
     const [menu, setMenu] = useState("shop");
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const { getTotalCartItems } = useContext(ShopContext);
+
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -45,7 +48,7 @@ const Navbar = () => {
                 <div className="nav-login-cart">
                     <Link to="/login"><button>Login</button></Link>
                     <Link to="/cart"><img src={cart_icon} alt="cart icon" /></Link>
-                    <div className="nav-cart-count">0</div>
+                    <div className="nav-cart-count">{getTotalCartItems()}</div>
                 </div>
             </div>
         </div>
