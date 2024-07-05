bd927a910a371fba0e522de087be9e24660509a5
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-07-05 11:43:52 +0800

    setup description box in product details

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navogator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadscrum from '../Components/Breadcrums/Breadcrum';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 
 const Product = () => {
     const { all_product } = useContext(ShopContext)
@@ -12,6 +13,7 @@ const Product = () => {
         <div>
             <Breadscrum product={product} />
             <ProductDisplay product={product} />
+            <DescriptionBox />
         </div>
     )
 }
