b5e4ca8eb9d8dbcad791b43ff9c3f60198ebf281
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-07-08 22:28:39 +0800

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
+// import './App.css';
+// import Navbar from './Components/Navbar/Navbar';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Shop from './Pages/Shop';
+// import ShopCategory from './Pages/ShopCategory';
+// import Product from './Pages/Product';
+// import Cart from './Pages/Cart';
+// import LoginSignup from './Pages/LoginSignup';
+// import Checkout from './Pages/Checkout';
+// import Footer from './Components/Footer/Footer';
+// import men_banner from './Components/Assets/banner_mens.png'
+// import women_banner from './Components/Assets/banner_women.png'
+// import kid_banner from './Components/Assets/banner_kids.png'
+
+// function App() {
+
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Shop />} />
+//           <Route path="/men" element={<ShopCategory banner={men_banner} category="men" />} />
+//           <Route path="/women" element={<ShopCategory banner={women_banner} category="women" />} />
+//           <Route path="/kid" element={<ShopCategory banner={kid_banner} category="kid" />} />
+//           <Route path="product" element={<Product />}>
+//             <Route path=":productId" element={<Product />} />
+//           </Route>
+//           <Route path="/cart" element={<Cart />} />
+//           <Route path="/login" element={<LoginSignup />} />
+//           <Route path="/checkout" element={<Checkout />} />
+//         </Routes>
+//         <Footer />
+//       </BrowserRouter>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// Update setup
+
+import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -8,12 +52,11 @@ import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Checkout from './Pages/Checkout';
 import Footer from './Components/Footer/Footer';
-import men_banner from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kid_banner from './Components/Assets/banner_kids.png'
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
-
   return (
     <div>
       <BrowserRouter>
@@ -23,9 +66,7 @@ function App() {
           <Route path="/men" element={<ShopCategory banner={men_banner} category="men" />} />
           <Route path="/women" element={<ShopCategory banner={women_banner} category="women" />} />
           <Route path="/kid" element={<ShopCategory banner={kid_banner} category="kid" />} />
-          <Route path="product" element={<Product />}>
-            <Route path=":productId" element={<Product />} />
-          </Route>
+          <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
           <Route path="/checkout" element={<Checkout />} />
@@ -33,7 +74,7 @@ function App() {
         <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
