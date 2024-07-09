80d3c61d4e8977f438c27ab00d2d689c6968c886
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-07-09 21:18:39 +0800

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-ecom/',
   plugins: [react()],
-});
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      src: path.resolve(__dirname, './src')
+    }
+  }
+})
