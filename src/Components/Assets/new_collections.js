b2d23da24ad0b002f2b23b00c63f72fccab008f9
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-08-12 12:53:24 +0800

    update the new_collections database

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -6,6 +6,11 @@ import p5_img from "./product_15.png";
 import p6_img from "./product_2.png";
 import p7_img from "./product_17.png";
 import p8_img from "./product_28.png";
+import p9_img from "./product_17.png";
+import p10_img from "./product_8.png";
+import p11_img from "./product_14.png";
+import p12_img from "./product_35.png";
+
 
 let new_collections = [
   {
@@ -63,6 +68,36 @@ let new_collections = [
     image: p8_img,
     new_price: 100.0,
     old_price: 150.0,
+  },
+  {
+    id: 7,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p9_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 10,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p10_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },  
+  {
+    id: 13,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p11_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 18,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p12_img,
+    new_price: 85.0,
+    old_price: 120.5,
   },
 ];
 
