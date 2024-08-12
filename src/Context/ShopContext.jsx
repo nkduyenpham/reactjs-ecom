f2b84d5b4bdd9301b0b47cb4cf0a11fe5303ddb9
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-08-12 11:29:40 +0800

    Fixing ESLint recommendation

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import all_product from "../Components/Assets/all_product"
 
 export const ShopContext = createContext(null)
